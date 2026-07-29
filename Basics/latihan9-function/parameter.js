2c258dcb27cecfb9ae86a7af5e02a5c3fdcfbb8f
Si Ocong <81248499+siocongs@users.noreply.github.com>
2026-07-30 03:01:52 +0700

    feat(javascript - basics): latihan 9 - add function exercises

---
+++ Basics/latihan9-function/parameter.js
@@ -0,0 +1,10 @@
+function tambah(a, b) {
+    return a + b;
+}
+function kali(a, b) {
+    return a * b;
+}
+// var a = 10;
+// var b = 20;
+var hasil = kali(tambah(3, 2), tambah(2, 2));
+console.log(hasil);
