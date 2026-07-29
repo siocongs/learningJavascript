c13d9b917962f67640669c277382f5dd4b79a7a7
Si Ocong <81248499+siocongs@users.noreply.github.com>
2026-07-30 02:57:42 +0700

    feat(javascript - basics): latihan 3 - add while loop exercises

---
+++ Basics/latihan3-looping while/warnet.js
@@ -0,0 +1,5 @@
+komputer = 1;
+while (komputer <= 10) {
+    console.log('Komputer No. ' + komputer + ' beroperasi dengan baik');
+komputer++;
+}
---
+++ Basics/latihan3-looping while/while.js
@@ -0,0 +1,15 @@
+// while (false) {
+//     console.log('Hello Earth!');
+// }
+
+// var ulang = true;
+// while (ulang) {
+//     console.log('Hello Earth!');
+//     ulang = confirm('Apakah anda ingin mengulang?');
+// }
+
+var nilaiAwal = 1;
+while (nilaiAwal <= 5) {
+    console.log('Hello Earth! ' + nilaiAwal + 'x');
+    nilaiAwal++;
+}
