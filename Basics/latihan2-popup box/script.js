11d4eba3753f3e61aab0ec62e1c50916a3ea56ac
Si Ocong <81248499+siocongs@users.noreply.github.com>
2026-07-30 02:57:19 +0700

    feat(javascript - basics): latihan 2 - add popup box interaction

---
+++ Basics/latihan2-popup box/script.js
@@ -0,0 +1,23 @@
+// alert('Hello World');
+// alert('hallo');
+
+// var nama = prompt('Input your name :');
+// alert(nama);
+
+// var konfirmasi = confirm('Are you sure?');
+// if (konfirmasi === true) {
+    //     alert('You are sure');
+    // } else {
+        //     alert('You are not ready');
+        // }   
+        
+        
+alert('Welcome Home Budyy!');
+var cobalagi = true;
+while (cobalagi === true) {
+    var nama = prompt('Input your name :');
+    alert('your name is ' + nama);
+
+    var cobalagi = confirm('Try again?');
+}
+alert('Goodbye');
